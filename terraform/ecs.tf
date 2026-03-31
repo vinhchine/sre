2e31533e1d5410e7d9c79c97369a2656c306b0e5
vinh <tranvuvinh2312@gmail.com>
2026-04-01 01:14:40 +0700

    clean repo1

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu    = "256"
   memory = "512"
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = "arn:aws:iam::<ACCOUNT_ID>:role/ecsTaskExecutionRole"
   network_mode = "awsvpc"
 
   container_definitions = jsonencode([
